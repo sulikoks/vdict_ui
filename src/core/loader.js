f56d13374a972e9bad53dc2621b51daa66a510ae
suliko <79033456672sd@gmail.com>
2022-12-25 22:42:45 +0800

    - added main loader

---
+++ src/core/loader.js
@@ -0,0 +1,25 @@
+class Loader {
+  id = "main-loader";
+  idApp = "app";
+  constructor() {
+    this.$elApp = document.getElementById(this.idApp);
+    this.$el = document.getElementById(this.id);
+
+    console.log(this.$elApp);
+    console.log(this.$el);
+  }
+
+  hide() {
+    if (this.$el) this.$el.style.opacity = "0";
+    if (this.$elApp) this.$elApp.style.opacity = "100%";
+    setTimeout(() => {
+      if (this.$el) this.$el.style.display = "none";
+    }, 350);
+  }
+
+  show() {
+    // this.$el.style.display = "unset";
+    // this.$elApp.style.display = "none";
+  }
+}
+export default new Loader();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
 import vuetify from "./core/vuetify";
+import loader from "./core/loader";
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount("#app")
+  .$nextTick()
+  .then(() => {
+    console.log("Hello");
+    loader.hide();
+  });
