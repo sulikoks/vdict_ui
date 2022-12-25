5a80c7aa7edf8cb5848c688d0f44cabc12fe754c
suliko <79033456672sd@gmail.com>
2022-12-25 21:39:19 +0800

    - upgraded vite up to 3.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import { VitePWA } from "vite-plugin-pwa";
 
+console.log("------->  process && process.env");
+const NODE_ENV = process.env.NODE_ENV;
+console.log(NODE_ENV);
+console.log(NODE_ENV !== "production");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,7 +17,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "auto",
       devOptions: {
-        enabled: true,
+        enabled: NODE_ENV !== "production",
       },
       includeAssets: [
         "favicon.ico",
